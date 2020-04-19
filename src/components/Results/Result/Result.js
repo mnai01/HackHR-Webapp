dc5f9d8c3e92e81fd25623ca8f8dd54e172c69dc
julian-hecker <julianheckerdev@gmail.com>
2020-04-19 13:16:27 -0400

    add circle indicators to result list

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -4,10 +4,13 @@ import classes from "./Result.module.scss";
 
 
 const Result = (props) => {
+  const red = props.result.RGB.r;
+  const green = props.result.RGB.g;
+  const blue = props.result.RGB.b;
   return (
     <li className={classes.result}>
       <span className={classes.amount}>{props.result.LiveCount} / {props.result.LimitRestriction}</span>
-      <h3>{props.result.Name}</h3>
+      <h3>{props.result.Name}<span className={classes.circle} style={{backgroundColor: `rgb(${red}, ${green}, ${blue})`}}></span></h3>
       <p>{props.result.Description}</p>
     </li>
   );
