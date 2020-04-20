70c4f99d0a276fc2f24bf9eb17f7fcd86c61c0fb
mnai01 <imatlak03@gmail.com>
2020-04-19 22:39:29 -0400

    times useEffect and axios

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Results from "../Results/Results";
 import Map from "../Map/Map";
 import classes from "./MainPage.module.scss";
+import axios from "axios";
 
 const MainPage = (props) => {
+  const [GeofenceAPI, setGeofence] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setInterval(function () {
+      _getRequest();
+    }, 50000);
+  }, []);
+
+  const _getRequest = () => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://hack-hr.herokuapp.com/api/get"
+      )
+      .then((res) => {
+        setGeofence(res.data.data);
+        setLoaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className={classes.main_container}>
       <div className={classes.map_container}>
-        <Map user={props.People} data={props.Geofence} />
+        <Map user={props.People} data={GeofenceAPI} />
       </div>
       <div className={classes.results_container}>
-        <Results data={props.Geofence} />
+        {loaded ? <Results data={GeofenceAPI} /> : <h1>loading...</h1>}
       </div>
     </div>
   );
